f2527726e3214b627e36fa434ef1221cb47ae972
Corey Ford <corey@coreyford.name>
2015-05-02 11:38:08 -0700

    xmonad.hs: keys for all workspaces

--- xmonad.hs
+++ xmonad.hs
@@ -109,11 +109,11 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = fromList $
     -- mod-[1..9], Switch to workspace N
     -- mod-shift-[1..9], Move client to workspace N
     [((m .|. modm, k), windows $ f i)
-        | (i, k) <- zip (XMonad.workspaces conf) ([xK_1 .. xK_4])
+        | (i, k) <- zip (XMonad.workspaces conf) ([xK_1 .. xK_9])
         , (f, m) <- [{-(W.greedyView, 0),-} (W.shift, shiftMask)]]
     ++
     [((modm, k), switchTopic myTopicConfig i)
-        | (i, k) <- zip (XMonad.workspaces conf) ([xK_1 .. xK_4])]
+        | (i, k) <- zip (XMonad.workspaces conf) ([xK_1 .. xK_9])]
     ++
 
     -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
