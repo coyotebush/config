90b35c52bd803372bd79ebffd4c04b8ea427ed59
Corey Ford <corey@coreyford.name>
2015-09-21 15:37:40 -0700

    prefer phillipberndt/autorandr to my own

--- xmonad.hs
+++ xmonad.hs
@@ -97,7 +97,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = fromList $
     , ((modm .|. mod1Mask,  xK_Print ), spawn "shoot")
     , ((modm,               xK_s     ), spawn "xscreensaver-command -lock")
     , ((modm .|. shiftMask, xK_s     ), spawn "systemctl suspend")
-    , ((modm .|. mod1Mask,  xK_s     ), spawn "autorandr")
+    , ((modm .|. mod1Mask,  xK_s     ), spawn "autorandr.py --change")
     , ((modm,               xK_b     ), spawnBrowser)
     , ((modm .|. shiftMask, xK_b     ), spawnFileBrowser)
     , ((modm .|. mod1Mask,  xK_b     ), spawn "btheadset")
